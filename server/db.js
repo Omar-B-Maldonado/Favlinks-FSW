f391fa375aeed545d72660be7bd1c9fb0d4e4767
Omar Maldonado <omar.maldonado1@lc.cuny.edu>
2023-12-13 23:06:25 -0500

    completed assignment part 2

--- server/db.js
+++ server/db.js
@@ -8,34 +8,53 @@ port: 5432,
 })
 
 const createLink = (req, res) => {
-    /*
-    const { url, description } = req.body;
-
-    // Validate input data
-    if (!url || !description) {
-        return res.status(400).json({ error: 'URL and description are required.' });
-    }
-
-    pool.query('INSERT INTO favlinks (url, description) VALUES ($1, $2) RETURNING *', [url, description], (error, result) => {
-        if (error) throw error
-        res.status(201).json(result.rows[0]);
+    
+    const { name , url } = req.body;
+    pool.query('INSERT INTO favlinks (name, url) VALUES ($1, $2) RETURNING *',
+    [name, url],
+     
+    (error, result) => {
+        if (error) throw error;
+        else res.status(201).json(result.rows[0]);
     });
-    */
 };
 
 const getLinks = (req, res) => {
-    pool.query('SELECT * FROM favlinks ORDER BY id ASC', (error, result) => {
-        if (error) throw error
-        res.status(200).json(result.rows)
+    
+    pool.query('SELECT * FROM favlinks ORDER BY id ASC',
+    
+    (error, result) => {
+        if (error) throw error;
+        else res.status(200).json(result.rows);
     })
 }
 
 const updateLink = (req, res) => {
     
+    const id = parseInt(req.params.id)
+    const { name, url } = req.body
+
+    pool.query('UPDATE favlinks SET name = $1, url = $2 WHERE id = $3',
+    [name, url, id],
+    
+    (error, result) => {
+        if (error) throw error;
+        else res.status(200).json(result.rows);
+    })
   };
   
-  const deleteLink = (req, res) => {
+const deleteLink = (req, res) => {
     
+    const id = parseInt(req.params.id)
+
+    pool.query('DELETE FROM favlinks WHERE id = $1',
+    [id], 
+
+    (error, result) =>{
+        if (error) throw error;
+        else res.status(200).json(result.rows)
+    })
+
   };
 
 module.exports = {
--- server/index.js
+++ server/index.js
@@ -4,8 +4,9 @@ const cors    = require('cors')
 const db      = require('./db')
 const app     = express()
 const port    = 3000
+const clientPath = path.resolve(__dirname, '../client/dist')
 
-app.use(express.json())
+app.use(express.static(clientPath))
 app.use(cors())
 
 
@@ -18,28 +19,9 @@ app.delete('/api/links/:id', db.deleteLink);//D
 app.use(express.static(path.resolve(__dirname, '../client/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
-});
+  res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'))
+    })
 
 app.listen(port, () => {
 console.log(`App running on port ${port}.`)
 })
-
-/*
-const express = require('express')
-const path    = require('path')
-const db      = require('./db')
-const app     = express();
-const PORT    = 3000
-const clientPath = path.resolve(__dirname, '../client/dist')
-app.use(express.static(clientPath))
-app.get('/api/links', db.getLinks)
-app.get('/', (req, res) => {
-    // we'll do some stuff here
-    res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'))
-    })
-
-    app.listen(PORT, () => {
-        console.log(`Server listening on port ${PORT}`)
-        })
-*/
